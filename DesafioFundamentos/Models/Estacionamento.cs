dadb4d8dfbbd9b625a8434199416342ca1708324
fflps <fabrielysantos15@gmail.com>
2024-01-08 01:12:03 -0300

    Desafio concluído!

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -30,7 +30,7 @@ namespace DesafioFundamentos.Models
             {
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
 
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
+                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado
                 int.TryParse(Console.ReadLine(), out int horas);
                 // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
                 decimal valorTotal = precoInicial + (precoPorHora * horas);
